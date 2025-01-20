ee8f9fca11a4321964cbfd8a128efc92dd770e0c
david_smith <david_smith@sweetwater.com>
2025-01-20 08:36:35 -0500

    feat/control-model-comment-visibility Updated `tests/TestCase.php`.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,10 +3,13 @@
 namespace Tests;
 
 use PHPUnit\Framework\TestCase as BaseTestCase;
+use Zerotoprod\DataModelGenerator\Engine;
+use Zerotoprod\DataModelGenerator\Models\Components;
 
 abstract class TestCase extends BaseTestCase
 {
     public static string $test_dir = './tests/generated';
+
     protected function setUp(): void
     {
         parent::setUp();
@@ -15,6 +18,16 @@ abstract class TestCase extends BaseTestCase
             (stripos(PHP_OS_FAMILY, 'WIN') === 0
                 ? 'rmdir /S /Q '
                 : 'rm -rf ').self::$test_dir
+        );
+    }
+
+    public function engineGenerate(string $dir = __DIR__): void
+    {
+        Engine::generate(
+            Components::from([
+                Components::Config => json_decode(file_get_contents($dir.'/data_model.json'), true),
+                ...json_decode(file_get_contents($dir.'/models.json'), true),
+            ]),
         );
     }
 }
