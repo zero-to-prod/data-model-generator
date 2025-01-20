03d2b4b84b761a0617ca060ca88fe00e53cf9e99
david_smith <david_smith@sweetwater.com>
2025-01-20 08:36:30 -0500

    feat/control-model-comment-visibility Updated `tests/Acceptance/Config/Constants/ExcludeType/Exclude/IncludeTest.php`.

--- tests/Acceptance/Config/Constants/ExcludeType/Exclude/IncludeTest.php
+++ tests/Acceptance/Config/Constants/ExcludeType/Exclude/IncludeTest.php
@@ -5,20 +5,13 @@ namespace Tests\Acceptance\Config\Constants\ExcludeType\Exclude;
 use PHPUnit\Framework\Attributes\Test;
 use Tests\TestCase;
 use Zerotoprod\DataModelGenerator\Engine;
-use Zerotoprod\DataModelGenerator\Models\Components;
-use Zerotoprod\DataModelGenerator\Models\Config;
 
 class IncludeTest extends TestCase
 {
     /** @link Engine::generate() */
     #[Test] public function generate(): void
     {
-        Engine::generate(
-            Components::from([
-                Components::Config => json_decode(file_get_contents(__DIR__.'/data_model.json'), true),
-                ...json_decode(file_get_contents(__DIR__.'/models.json'), true),
-            ]),
-        );
+        $this->engineGenerate(__DIR__);
 
         self::assertStringEqualsFile(
             expectedFile: self::$test_dir.'/User.php',
