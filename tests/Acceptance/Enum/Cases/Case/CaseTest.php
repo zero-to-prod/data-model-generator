bdf86bde01214eeee37ef59c7fa5f53678de2742
david_smith <david_smith@sweetwater.com>
2025-01-20 08:36:32 -0500

    feat/control-model-comment-visibility Updated `tests/Acceptance/Enum/Cases/Case/CaseTest.php`.

--- tests/Acceptance/Enum/Cases/Case/CaseTest.php
+++ tests/Acceptance/Enum/Cases/Case/CaseTest.php
@@ -5,19 +5,13 @@ namespace Tests\Acceptance\Enum\Cases\Case;
 use PHPUnit\Framework\Attributes\Test;
 use Tests\TestCase;
 use Zerotoprod\DataModelGenerator\Engine;
-use Zerotoprod\DataModelGenerator\Models\Components;
 
 class CaseTest extends TestCase
 {
     /** @link Engine::generate() */
     #[Test] public function generate(): void
     {
-        Engine::generate(
-            Components::from([
-                Components::Config => json_decode(file_get_contents(__DIR__.'/data_model.json'), true),
-                ...json_decode(file_get_contents(__DIR__.'/models.json'), true),
-            ]),
-        );
+        $this->engineGenerate(__DIR__);
 
         self::assertStringEqualsFile(
             expectedFile: self::$test_dir.'/Role.php',
